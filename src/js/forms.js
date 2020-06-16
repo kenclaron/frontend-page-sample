5da02ac93de35a33b4daf3db9a19445f083ffc3e
kirusanov <kirusanov@gmail.com>
2020-06-16 12:12:56 +0400

    css: add base components js: .js-label

--- src/js/forms.js
+++ src/js/forms.js
@@ -7,11 +7,31 @@ let forms = {
         forms.app = this;
 
         this.document.ready(() => {
+            forms.initInputLabel();
             forms.initMask();
         });
     },
+
+    initInputLabel() {
+        let sel = '.js-label input:not([required]), .js-label textarea:not([required])';
+        let className = '_label-empty';
+        this.app.document
+            .on('focus', sel, function () {
+                $(this).siblings('label').removeClass(className);
+            })
+            .on('blur', sel, function () {
+                if (!$(this).val()) {
+                    $(this).siblings('label').addClass(className);
+                }
+            });
+        $(sel).each(function () {
+            if (!$(this).val()) {
+                $(this).siblings('label').addClass(className);
+            }
+        });
+    },
     
-    initMask: function () {
+    initMask() {
         let selector = document.querySelectorAll('.js-mask__tel');
         Inputmask({
             mask: '+7 (999) 999 99 99',
